ec51cb4f0b5f4b616038df561d490369d180f51f
Thomas Santos <thomas@socialeffort.co>
2017-09-23 23:17:03 -0400

    add server.js

--- ToDoApi/server.js
+++ ToDoApi/server.js
@@ -1,8 +1,28 @@
 // require express
+// connect database
+// load created task
+// use bodyParser
+// register created routes in server
+
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 9001;
+  port = process.env.PORT || 9001,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel.js'), //grabs the pre-created model
+  bodyParser = require('body-parser');
+
+// mongoose connects to url connection
+// an async method
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
 app.listen(port);
 
 console.log('ToDo RESTful API Server initialised on: ' + port);
+
