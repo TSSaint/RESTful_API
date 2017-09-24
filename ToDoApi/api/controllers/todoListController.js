253d9b17825d141d10b04a5b8a9aadc7b149cca7
Thomas Santos <thomas@socialeffort.co>
2017-09-23 23:00:28 -0400

    add controllers using native mongoose methods

---
+++ ToDoApi/api/controllers/todoListController.js
@@ -0,0 +1,56 @@
+// define functions for export into routes.
+// each one uses native mongoose-defined methods
+
+'use strict';
+
+var mongoose = require('mongoose'),
+  Task = mongoose.model('Tasks');
+
+// list all tasks
+exports.list_all_tasks = function(req, res) {
+  Task.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+// create a new task
+exports.create_a_task = function(req, res) {
+  var new_task = new Task(req.body);
+  new_task.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+// view a new task
+exports.read_a_task = function(req, res) {
+  Task.findById(req.params.taskId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+// edit information in an existing task
+exports.update_a_task = function(req, res) {
+  Task.findOneAndUpdate(
+    {_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+// destroy instance of a task
+exports.delete_a_task = function(req, res) {
+  Task.remove({
+    _id: req.params.taskId
+  }, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Task successfully deleted' });
+  });
+};
